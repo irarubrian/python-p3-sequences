2883c602c0105a9c9d3d6c14c0634510804bd7e4
irarubrian <brian.o.iraru@gmail.com>
2025-02-14 16:34:29 +0300

    commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])  # Edge case: if length is 0 or negative, return an empty list
+        return
+    
+    fibonacci = [0, 1]  # Start with the first two numbers
+    for _ in range(2, length):  # Generate remaining numbers
+        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+    
+    print(fibonacci[:length])  # Print only up to the given length
+
+
